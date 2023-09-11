6c650a54ad259d7ddd6d1793e97850f9250d42c2
thesiddharth24 <97101731+thesiddharth24@users.noreply.github.com>
2023-09-11 14:18:24 +0530

    events projects completed

---
+++ 6-unlimitedColors/chaiaurcode.js
@@ -0,0 +1,32 @@
+//generate a random color
+
+const randomColor = function(){
+  const hex="0123456789ABCDEF"
+  let color='#'
+
+  for(let i =0;i<6;i++){
+    color+= hex[Math.floor(Math.random()*16)]
+  }
+
+  return color;
+}
+
+//console.log(randomColor());
+const changeBGColor = function(){
+  document.body.style.backgroundColor=randomColor();
+}
+let intervalID;
+const startChangingColor = function(){
+  if(!intervalID){
+   intervalID = setInterval(changeBGColor,1000)
+  }
+   
+}
+const stopChangingColor = function(){
+   clearInterval(intervalID);
+   intervalID=null;
+}
+
+document.querySelector("#start").addEventListener('click',startChangingColor);
+
+document.querySelector("#stop").addEventListener('click',stopChangingColor)
